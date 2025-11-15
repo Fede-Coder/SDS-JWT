e4c82bd28d16ea37db00a3fc60773c7ec5957375
Fede-Coder <federiconieto04@gmail.com>
2025-11-15 19:17:13 -0300

    Fix: local strategy

--- backend/src/solution/auth/local/local.strategy.ts
+++ backend/src/solution/auth/local/local.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 import { User } from 'src/user/user.entity';
-import { SignInDto } from '../dto/sign-in.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
@@ -13,11 +12,8 @@ export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
         });
     }
 
-    async validate(dto: SignInDto): Promise<User> {
-        const result = await this.authService.validateUser(
-            dto.email,
-            dto.password,
-        );
+    async validate(email: string, password: string): Promise<User> {
+        const result = await this.authService.validateUser(email, password);
 
         if (result?.error || !result.payload) {
             throw new UnauthorizedException(result?.message || 'Unauthorized');
